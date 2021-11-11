5114742ffe360de6131e51e4e4a1bc2c73057fb6
Bradford Barclay <91262137+barclaybNLCC@users.noreply.github.com>
2021-11-11 15:07:20 +0000

    cleaning up barclay branch

--- main.py
+++ main.py
@@ -9,64 +9,15 @@ class Actor():
   #send a message to another actor
   #put/use encrypt in the send function
   def send(self, Actor):
-    
-    file.append(ciphertext)
-    
-    if Actor==Alice:
-      aliceTargetMailBox.append(file)
-    elif Actor==Bob:
-      bobTargetMailBox.append(file)
-    elif Actor==Chuck:
-      chuckTargetMailBox.append(file)
-    else:
-      print("Something went wrong...try again later")
-    #ben
-
-
-
-             #!!!!!open dm's needs to be made as a new function!!!!!
-    
-  
-  ALPHABET = 'abcdefghijklmnopqrstuvwxyz.,?:1234567890' 
-
-  def get_plaintext():
-    plaintext = input('Please type your message ')
-    return plaintext.lower()  
+    pass
 
   #encrypt the plaintext of the message, using a keyphrase
   def encrpyt(keyphrase):
-    ciphertext = ''
-    for position, character in enumerate(plaintext):
-      if character not in ALPHABET:
-        ciphertext += character
-      else:
-            encrypted = (ALPHABET.index(character) + enumerate(keyphrase))
-            ciphertext += ALPHABET[encrypted]
-    return ciphertext
-    #ethan 
+    pass
 
-
-
-
-    ALPHABET = 'abcdefghijklmnopqrstuvwxyz'
-
-  def get_plaintext():
-    plaintext = input('Please type your message ')
-    return plaintext.lower()  
-
-  
   #decrypt the ciphertext 
   def decrypt(message,keyphrase):
-      ciphertext = ''
-    for position, character in enumerate(plaintext):
-      if character not in ALPHABET:
-        ciphertext += character
-      else:
-            encrypted = (ALPHABET.index(character) + int(sheet[position])) % 40
-            ciphertext += ALPHABET[encrypted]
-    return ciphertext
-    #elisha
- 
+    pass
  #A message that can be sent by Actors
 class Message():
   def __init__(self):

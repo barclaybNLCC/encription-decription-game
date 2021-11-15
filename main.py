ac63f1067138b6db35f23b84a9c27afedc4bf81b
BenLDonk <94182960+BenLDonk@users.noreply.github.com>
2021-11-15 14:54:51 +0000

    elishas code

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+#this is ben's branch
+
+
 #An actor in our scenario()
-#test
-#test 
 class Actor():
   def __init__(self, privateKey, publicKey):
     self.privateKey=privateKey
@@ -11,17 +12,65 @@ class Actor():
   #send a message to another actor
   #put/use encrypt in the send function
   def send(self, Actor):
-    pass
     
+    file.append(ciphertext)
+    
+    if Actor==Alice:
+      aliceTargetMailBox.append(file)
+    elif Actor==Bob:
+      bobTargetMailBox.append(file)
+    elif Actor==Chuck:
+      chuckTargetMailBox.append(file)
+    else:
+      print("Something went wrong...try again later")
+    #ben
+
+
+
+             #!!!!!open dm's needs to be made as a new function!!!!!
+    
+  
+  ALPHABET = 'abcdefghijklmnopqrstuvwxyz.,?:1234567890' 
+
+  def get_plaintext():
+    plaintext = input('Please type your message ')
+    return plaintext.lower()  
+
   #encrypt the plaintext of the message, using a keyphrase
   def encrpyt(keyphrase):
-    pass
+    ciphertext = ''
+    for position, character in enumerate(plaintext):
+      if character not in ALPHABET:
+        ciphertext += character
+      else:
+            encrypted = (ALPHABET.index(character) + enumerate(keyphrase))
+            ciphertext += ALPHABET[encrypted]
+    return ciphertext
+    #ethan 
+
+
+
+
+    ALPHABET = 'abcdefghijklmnopqrstuvwxyz'
+
+  def get_plaintext():
+    plaintext = input('Please type your message ')
+    return plaintext.lower()  
+
   
   #decrypt the ciphertext 
   def decrypt(message,keyphrase):
-     pass
-
-#A message that can be sent by Actors
+      ciphertext = ''
+    for position, character in enumerate(plaintext):
+      if character not in ALPHABET:
+        ciphertext += character
+      else:
+            encrypted = (ALPHABET.index(character) + int(sheet[position])) % 40
+            ciphertext += ALPHABET[encrypted]
+    return ciphertext
+    #elisha
+ 
+ #A message that can be sent by Actors
 class Message():
   def __init__(self):
     #the encrypted cleartext

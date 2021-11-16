886c77dc18842a30badbeb1a4c23508c5b585f11
Bradford Barclay <91262137+barclaybNLCC@users.noreply.github.com>
2021-11-16 14:25:54 +0000

    writing the keys to a local .pem file.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives import serialization
 
 print("test")
 
@@ -47,7 +48,13 @@ private_key = rsa.generate_private_key(
 
 public_key = private_key.public_key()
 
-print(private_key)
-print(public_key)
-print(dir(public_key))
-print("hello world")
+
+
+pem = private_key.private_bytes(
+    encoding=serialization.Encoding.PEM,
+    format=serialization.PrivateFormat.PKCS8,
+    encryption_algorithm=serialization.NoEncryption()
+)
+
+with open('private_key.pem', 'wb') as f:
+    f.write(pem)

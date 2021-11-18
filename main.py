bd328680a405827ffb8831588e6b58188e2a0313
BenLDonk <94182960+BenLDonk@users.noreply.github.com>
2021-11-18 14:42:32 +0000

    culled some code beccause ethan nedded to use it in his

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 #que
 que=[]
-l2 = []
-if not l2:
+server = []
+if not server:
     pass
-else:que.append(12)
-#open mail box
-if openMailBox is True:
-  print(que)
-#view decrypted messages
+else:que.append(server)

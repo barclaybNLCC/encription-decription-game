84c2232e4ebbc92e355c63cfd65439d27be9578b
BenLDonk <94182960+BenLDonk@users.noreply.github.com>
2021-11-18 15:10:58 +0000

    delete is done i think

--- main.py
+++ main.py
@@ -13,5 +13,12 @@ def send(reciever, cipher):
   server = server.pop(0)
 
 #delete
-print("If you would like to go to the home screen then type 0 and press enter")
-  answer=input("Would you like to send a message?")
+def delete(reciever):
+ print("If you would like to go to the home screen then type 0 and press enter")
+ answer=input("Would you like to send a message?Y/N\n")
+ if answer=="Y":
+   reciever.pop(0)
+ elif answer=="0":
+   choice=='0'
+ else:
+    pass

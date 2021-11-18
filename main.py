a77270b6ddf83a86bed8fb9a394136af63387c15
BenLDonk <94182960+BenLDonk@users.noreply.github.com>
2021-11-18 15:07:16 +0000

    finished send i think

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ else:que.append(server)
 #send
 def send(reciever, cipher):
   server.append(cipher)
-  cipher==[]
+  cipher = cipher.pop(0)
   reciever.append(server)
-  server=[]
+  server = server.pop(0)
+
+#delete
+print("If you would like to go to the home screen then type 0 and press enter")
+  answer=input("Would you like to send a message?")
